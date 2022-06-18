df863f3a9290b09d230d4cd9b51171f59d02e312
Teknath <jhateknath64@gmail.com>
2022-06-18 12:57:04 +0530

    added Edit profile | Phone

--- frontend/src/component/User/UpdateProfile.js
+++ frontend/src/component/User/UpdateProfile.js
@@ -9,128 +9,142 @@ import { useNavigate } from "react-router-dom";
 import { UPDATE_PROFILE_RESET } from "../../constants/userConstants";
 import MetaData from "../layout/MetaData";
 import "./SCSS/UpdateProfile/UpdateProfile.css";
-
+import PhoneIcon from "@mui/icons-material/Phone";
 
 const UpdateProfile = () => {
-    const dispatch = useDispatch();
-    const alert = useAlert();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const navigate = useNavigate();
 
-    const { user } = useSelector((state) => state.user);
-    const { error, isUpdated, loading } = useSelector((state) => state.profile);
+  const { user } = useSelector((state) => state.user);
+  const { error, isUpdated, loading } = useSelector((state) => state.profile);
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [avatar, setAvatar] = useState();
-    const [avatarPreview, setAvatarPreview] = useState("/Profile.png");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [avatar, setAvatar] = useState();
+  const [avatarPreview, setAvatarPreview] = useState("/Profile.png");
 
-    const updateProfileSubmit = (e) => {
-        e.preventDefault();
+  const updateProfileSubmit = (e) => {
+    e.preventDefault();
 
-        const myForm = new FormData();
+    const myForm = new FormData();
 
-        myForm.set("name", name);
-        myForm.set("email", email);
-        myForm.set("avatar", avatar);
-        dispatch(updateProfile(myForm));
+    myForm.set("name", name);
+    myForm.set("email", email);
+    myForm.set("phone", phone);
+    myForm.set("avatar", avatar);
+    dispatch(updateProfile(myForm));
+  };
+
+  const updateProfileDataChange = (e) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setAvatarPreview(reader.result);
+        setAvatar(reader.result);
+      }
     };
 
-    const updateProfileDataChange = (e) => {
-        const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
-        reader.onload = () => {
-            if (reader.readyState === 2) {
-                setAvatarPreview(reader.result);
-                setAvatar(reader.result);
-            }
-        };
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setPhone(user.phone);
+      setAvatarPreview(user.avatar.url);
+    }
 
-        reader.readAsDataURL(e.target.files[0]);
-    };
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
 
-    useEffect(() => {
-        if (user) {
-            setName(user.name);
-            setEmail(user.email);
-            setAvatarPreview(user.avatar.url);
-        }
+    if (isUpdated) {
+      alert.success("Profile Updated Successfully");
+      dispatch(loadUser());
 
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors());
-        }
+      navigate("/account");
 
-        if (isUpdated) {
-            alert.success("Profile Updated Successfully");
-            dispatch(loadUser());
+      dispatch({
+        type: UPDATE_PROFILE_RESET,
+      });
+    }
+  }, [dispatch, error, alert, navigate, user, isUpdated]);
 
-            navigate("/account");
-
-            dispatch({
-                type: UPDATE_PROFILE_RESET,
-            });
-        }
-    }, [dispatch, error, alert, navigate, user, isUpdated]);
-
-
-    return (
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
         <>
-            {loading ? (<Loader /> ) : (
-                <>
-                    <MetaData title="Update Profile -- BestShop" />
-                    <div className="updateProfileContainer">
-                        <div className="updateProfileBox">
-                            <h2 className="updateProfileHeading">Update Profile</h2>
+          <MetaData title="Update Profile -- BestShop" />
+          <div className="updateProfileContainer">
+            <div className="updateProfileBox">
+              <h2 className="updateProfileHeading">Update Profile</h2>
 
-                            <form
-                                className="updateProfileForm"
-                                encType="multipart/form-data"
-                                onSubmit={updateProfileSubmit}
-                            >
-                                <div className="updateProfileName">
-                                    <FaceIcon />
-                                    <input
-                                        type="text"
-                                        placeholder="Name"
-                                        required
-                                        name="name"
-                                        value={name}
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
-                                </div>
-                                <div className="updateProfileEmail">
-                                    <MailOutlineIcon />
-                                    <input
-                                        type="email"
-                                        placeholder="Email"
-                                        required
-                                        name="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                </div>
-
-                                <div id="updateProfileImage">
-                                    <img src={avatarPreview} alt="Avatar Preview" />
-                                    <input
-                                        type="file"
-                                        name="avatar"
-                                        accept="image/*"
-                                        onChange={updateProfileDataChange}
-                                    />
-                                </div>
-                                <input
-                                    type="submit"
-                                    value="Update"
-                                    className="updateProfileBtn"
-                                />
-                            </form>
-                        </div>
-                    </div>
-                </>
-            )}
+              <form
+                className="updateProfileForm"
+                encType="multipart/form-data"
+                onSubmit={updateProfileSubmit}
+              >
+                <div className="updateProfileName">
+                  <FaceIcon />
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    required
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="updateProfileEmail">
+                  <MailOutlineIcon />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="updateProfileEmail">
+                  <PhoneIcon />
+                  <input
+                    type="text"
+                    placeholder="Phone"
+                    required
+                    name="phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </div>
+                <div id="updateProfileImage">
+                  <img src={avatarPreview} alt="Avatar Preview" />
+                  <input
+                    type="file"
+                    name="avatar"
+                    accept="image/*"
+                    onChange={updateProfileDataChange}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Update"
+                  className="updateProfileBtn"
+                />
+              </form>
+            </div>
+          </div>
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default UpdateProfile;
