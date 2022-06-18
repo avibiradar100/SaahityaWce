1cde628820db80ed87d0a5c44a9d1b616ec83702
Avinash Biradar <biradarav.100@gmail.com>
2022-06-19 02:41:05 +0530

    added images carousel

--- frontend/src/component/Home/CarouselHome.js
+++ frontend/src/component/Home/CarouselHome.js
@@ -7,7 +7,7 @@ const CarouselHome = () => {
     <Carousel autoplay={true} autoplayInterval={2000} pauseOnHover={false} wrapAround={true} dragging={true} renderCenterLeftControls={null} renderCenterRightControls={null}>
       <img src="https://res.cloudinary.com/avicloud/image/upload/v1655528662/network-connection-graphic-overlay-background_lxfcry.jpg" alt='1' />
       <img src="https://res.cloudinary.com/avicloud/image/upload/c_mfit,h_310/v1655478069/bg1_ayfdxw.png" alt='2' />
-      <img src="https://res.cloudinary.com/avicloud/image/upload/v1655586373/medium-shot-girl-reading-book_qnr5pn.jpg" alt='2' />
+      <img src="https://res.cloudinary.com/avicloud/image/upload/v1655586373/medium-shot-girl-reading-book_qnr5pn.jpg" alt='3' />
       <img src="https://res.cloudinary.com/avicloud/image/upload/v1655474644/b2_xyivsg.jpg" alt='4' />
       <img src="https://res.cloudinary.com/avicloud/image/upload/v1655529303/back-school-background-with-school-supplies-copy-space-notebook_n5pwss.jpg" alt='5' />
       <img src="https://res.cloudinary.com/avicloud/image/upload/v1655528119/g21_kvkkte.jpg" alt='6' />
